d16bcf23a2f431c872f9079118eb72d6e50bccf8
Samuel Ayinde <ayindesamuel0@gmail.com>
2022-09-19 21:53:00 +0100

    fix 4 print rev

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -13,5 +13,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _putchar(char c);
 
 #endif
