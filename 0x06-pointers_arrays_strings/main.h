8d2de17db3b4e4f36a2f1ee0fcc2641751a296ca
Samuel Ayinde <ayindesamuel0@gmail.com>
2022-09-22 20:51:40 +0100

    finish basic tasks

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,10 @@ int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strcat(char*, char*);
 char *_strncpy(char*, char*, int);
+int _strcmp(char*, char*);
+void reverse_array(int*, int);
+char *string_toupper(char*);
+char *cap_string(char*);
+char *leet(char *);
 
 #endif
